3eb13fa1d6158b06556a95a67aa5384ecd7264da
kuhafsoiusygdfouysd <fhardy25@susdgapps.org>
2026-02-06 11:47:58 -0700

    Refactor server to remove bare-server integration

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
 const http = require('http');
-const { createServer } = require('@tomphttp/bare-server-node');
+const https = require('https');
+const url = require('url');
 
-const server = http.createServer();
-const bareServer = createServer('/bare/');
-
-server.on('request', (request, response) => {
-  if (bareServer.shouldRoute(request)) {
-    bareServer.routeRequest(request, response);
-  } else {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('BARE Server is running!');
-  }
-});
-
-server.on('upgrade', (request, socket, head) => {
-  if (bareServer.shouldRoute(request)) {
-    bareServer.routeUpgrade(request, socket, head);
-  } else {
-    socket.end();
-  }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('BARE Server is running!');
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`BARE Server running on http://localhost:${PORT}`);
-  console.log(`Bare endpoint: http://localhost:${PORT}/bare/`);
+  console.log(`Server running on port ${PORT}`);
 });
